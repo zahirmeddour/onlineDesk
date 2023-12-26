feef9f257cd6382e37e3715371df94fa2b7de148
zahirmeddour <zahir.meddour.88@gmail.com>
2023-12-26 12:02:50 +0100

    refactor mysql.class.php

--- mysql.class.php
+++ mysql.class.php
@@ -4,10 +4,10 @@ require_once("config.php");
 
 class My
 {
-    private $db_host = $config_db_host;
-    private $db_username = $config_db_username;
-    private $db_password = $config_db_password;
-    private $db_name = $config_db_name;
+    private $db_host = "localhost";
+    private $db_username = "root";
+    private $db_password = "";
+    private $db_name = "onlineDesktop";
     private $link = null;
 
     public function myqsldb_Connect()
